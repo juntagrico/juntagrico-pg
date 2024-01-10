7e272e68fc2004fc4ae1df7358fe36e039c332c6
pascalfree <pascal.free@ihdg.ch>
2024-01-10 03:41:24 +0100

    cleanup and improvements

--- juntagrico_pg/juntagricoapp.py
+++
@@ -1,9 +0,0 @@
-from juntagrico.util import addons
-
-
-def show_admin_menu(user):
-    return user.has_perm('juntagrico_pg.can_sql')
-
-
-addons.config.register_admin_menu('jpg/menu.html')
-addons.config.register_show_admin_menu_method(show_admin_menu)
